36c9011e50392efa1f9c0a633965ad0ca185fd09
Swarali <swaralimore3@gmail.com>
2026-03-28 20:47:05 +0530

    Day1: Add home route

---
+++ practice/day1.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#route 1 - Home
+@app.get("/")
+def home():
+    return {"message":"Hello World!"}
+
+#route 2 - About
+
+#@app.get("/about")
+
+#def about():
+    return{"message:" "I am learning FastAPI!"}
