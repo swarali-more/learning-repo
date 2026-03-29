4375e41f9de566107e96cca95482ed96e70b2368
Swarali <swaralimore3@gmail.com>
2026-03-29 20:12:41 +0530

    Day2: Add query parameter tasks

--- practice/day2.py
+++ practice/day2.py
@@ -15,27 +15,50 @@ def get_product():
 def get_product(product_id : int):
     return {"product_id": product_id}
 
+#Task: 1
 #Roll No.
 @app.get("/student/{roll_no}")
 def get_rollno(roll_no : int):
     return {"Roll_No": roll_no}
 
+#Task: 2
 #City_Name
 @app.get("/city/{city_name}")
 def get_city_name(city_name : str):
     return {"City Name" : city_name}
 
+#Task: 3
 #User_Id_Profile
 @app.get("/user/{user_id}/profile")
 def get_user_id(user_id : int):
     return {"User Info":f"Profile of user {user_id}"}
 
 
+
+
 #Query Parameter
-#@app.get("/search")
-#def search(name: str):
+@app.get("/search")
+def search(name: str):
     return {"search": name}
 
+#Query Parameter With Defaul Value
 @app.get("/search")
-def search(name: str = "Guest"):
-    return {"search": name}
+def search(name: str = "Guest"):  #default value
+    return {"search": name}       #our value
+
+#Item Limits
+@app.get("/items")
+def get_items(limit: int = 10,skip: int = 0):
+    return {"Limit": limit, "Skip": skip}
+
+#Task: 1
+#Students Class and Subjects
+@app.get("/students")
+def get_students(collage_name: str , subject: str = "all"):
+    return {"Collage_Name":collage_name,"Subject":subject}
+
+#Task: 2
+#Products
+@app.get("/product")
+def get_product(category: str,price: int = 100,discount: bool = False):
+    return {"Category":category, "Price": price ,"Discount": discount}
