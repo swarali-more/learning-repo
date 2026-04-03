84bf6f6abb9915ba80923c08d27f0c2e8e5d6114
Swarali <swaralimore3@gmail.com>
2026-04-03 12:05:25 +0530

    feat: add path and query parameter routes

--- fastapi-learning/practice/01_basic_routes/routes.py
+++ fastapi-learning/practice/01_basic_routes/routes.py
@@ -11,3 +11,28 @@ def home():
 @app.get("/about")
 def about():
     return{"message:" "I am learning FastAPI!"}
+
+# Route 3 - Users (Path Parameter)
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id}
+
+# Route 4 - Books (Path Parameter)
+@app.get("/book/{book_name}")
+def get_book(book_name: str):
+    return {"book_name": book_name}
+
+# Route 5 - Products (Query Parameter)
+@app.get("/products")
+def get_products(category: str, price: int):
+    return {"category": category, "price": price}
+
+# Route 6 - Students (Query Parameter)
+@app.get("/students")
+def get_students(name: str, age: int):
+    return {"name": name, "age": age}
+
+# Route 7 - Orders (Both - Path + Query)
+@app.get("/order/{order_id}")
+def get_order(order_id: int, status: str):
+    return {"order_id": order_id, "status": status}
