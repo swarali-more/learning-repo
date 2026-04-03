beb2667d12313338450c7e4437bc16b93d40e6ef
Swarali <swaralimore3@gmail.com>
2026-04-03 12:31:07 +0530

    test: add test cases for all basic routes

--- fastapi-learning/practice/tests/test_01_basic_routes.py
+++ fastapi-learning/practice/tests/test_01_basic_routes.py
@@ -20,3 +20,33 @@ def test_home():
 def test_about():
     response = client.get("/about")
     assert response.status_code == 200
+
+# Test 3 - User (Path Parameter)
+def test_get_user():
+    response = client.get("/user/5")
+    assert response.status_code == 200
+    assert response.json() == {"user_id": 5}
+
+# Test 4 - Book (Path Parameter)
+def test_get_book():
+    response = client.get("/book/Harry Potter")
+    assert response.status_code == 200
+    assert response.json() == {"book_name": "Harry Potter"}
+
+# Test 5 - Products ( Query Parameter)
+def test_get_products():
+    response = client.get("/products?category=electronics&price=500")
+    assert response.status_code == 200
+    assert response.json() == {"category": "electronics", "price": 500}
+
+# Test 6 - Students ( Query Parameter)
+def test_get_students():
+    response = client.get("/students?name=Swarali&age=20")
+    assert response.status_code == 200
+    assert response.json() == {"name": "Swarali", "age": 20}
+
+# Test 7 - Orders (Both (Path + Query))
+def test_get_order():
+    response = client.get("/order/101?status=delivered")
+    assert response.status_code == 200
+    assert response.json() == {"order_id": 101, "status": "delivered"}
